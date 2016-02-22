d2c580dd671842d984faa5eb3a3ed6eee429952f
amaidzia <amaidzia@hotmail.com>
2016-02-21 22:27:38 -0500

    Empty controller file
    Controller logic to be added

---
+++ Controller/Controller.m
@@ -0,0 +1,23 @@
+
+%%*************************************************************************
+%   Arduino_Outputs = Controller(Cost_Outputs)
+%  - Main controller program + calculates the outputs to send to arduino
+%  - Cost_Outputs = [ang,length,pressure,flow]  
+%  - Arduino_Outputs = [c1,c2,s1,s2,s3,s4,v1,v2,v3,v4],
+%  c=coup,v=value,s=solenoid
+% 
+% 
+% 
+%**************************************************************************
+
+
+
+
+function Arduino_Outputs = Controller(Cost_Outputs)
+
+    
+
+
+
+
+end
