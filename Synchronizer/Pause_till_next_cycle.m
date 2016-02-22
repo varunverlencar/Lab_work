aaca4d95410f3411a93f1ae5bfcd3e5abe01d04f
amaidzia <amaidzia@hotmail.com>
2016-02-21 22:30:41 -0500

    Added synchronization module
    Added sync module. This module judges when to start the timer and the
    next cycle

---
+++ Synchronizer/Pause_till_next_cycle.m
@@ -0,0 +1,20 @@
+
+%%*************************************************************************
+%   Pause_till_next_cycle(i)
+%  
+%  - This function waits till the next cycle
+% 
+% 
+% 
+% 
+%**************************************************************************
+
+
+
+
+function  Pause_till_next_cycle(i)
+    
+    while (toc< Time_period*i )
+    end
+
+end
