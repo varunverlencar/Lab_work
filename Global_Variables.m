2574fb89156d7eec0efdd0e879a7b1cd11aba11c
amaidzia <amaidzia@hotmail.com>
2016-02-21 22:29:19 -0500

    Added global Time_period
    Added global Time_period

--- Global_Variables.m
+++ Global_Variables.m
@@ -8,9 +8,14 @@
 % *************************************************************************
 
 
-%% TODO: create a global serial object
 
 
 s = serial('COM7');
 set(s,'BaudRate',115200);
 fopen(s);
+
+
+global Time_period;
+Time_period = 0.05; % Time period of 1 cycle
+
+
