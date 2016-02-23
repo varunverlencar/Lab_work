4c3e353e858edd7540375f90eeb81caaf105aafb
ajonnavittula <ajonnavittula@wpi.edu>
2016-02-23 14:23:47 -0500

    Added files via upload

--- Data_acq/Data_acq.ino
+++ Data_acq/Data_acq.ino
@@ -2,73 +2,66 @@
 char inData[20]; // Allocate some space for the string
 char inChar=-1; // Where to store the character read
 byte index = 0; // Index into array; where to store the character
-String str; 
- void setup() {
+String str,an="ang",le="len",fl="flo",pr="pre"; 
+
+ void setup() 
+ {
   // put your setup code here, to run once:
- Serial.begin(115200);
+ Serial.begin(115200,SERIAL_8N1);
  while(!Serial);
- Serial.println("serial port ready");
+// Serial.println("serial port ready");
+  an +='\n';
+  le +='\n';
+  fl +='\n';
+  pr +='\n'; 
+ }
  
-}
-char Comp(char* This) {
-    byte index = 0;
-    if (Serial.available() > 0) // Don't read unless                               // there you know there is data
-    {
-        str = Serial.readStringUntil('\n');
-//        x = Serial.parseInt();
-//        if(index < 19) // One less than the size of the array
-//        {
-//            inChar = Serial.read(); // Read a character
-//            inData[index] = inChar; // Store it
-//            index++; // Increment where to write next
-//            inData[index] = '\0'; // Null terminate the string
-//        }
-    Serial.println(str);
-    str.toCharArray(inData,3);
-    if (strcmp(inData,This)  == 0) {
-      for (int i=0;i<19;i++) 
-      {
-            inData[i]=0;
-      }
-        index=0;
-        return(0);
-    }
-    else {
-        return(1);
-    }
-}
-}
 void loop()
-{        
-        if(Comp("ang")==0) 
+{       
+        while (Serial.available() > 0) // Don't read unless                               // there you know there is data
+        {
+        str = Serial.readString();
+//        Serial.println(str);
+        }
+        if(an==str) 
           {
-            int out_angle=get_angle();
-//            Serial.write(out_angle); 
+           int out_angle=get_angle();
+//           Serial.println("out_angle"); 
+           Serial.write(out_angle);
+           Serial.println();
           }
-        if(Comp("len")==0) 
+        if(le==str) 
           {
             int out_length[10],test;
             test=65;
+//            Serial.println("out_length");
             for(int i=0;i<=10;i++)
             {
             out_length[i]=get_length();
-             
+            Serial.write(out_length[i]/256);
+            Serial.write(out_length[i]%256);
+            Serial.print('\n');
             }
 //          Serial.write(test);
           }
  
-        if(Comp("pre")==0) 
+        if(pr==str) 
           {
+//            Serial.println("out_pre");
             int out_pres=get_pres();
-//            Serial.write(out_pres); 
+//            Serial.write(out_pres,2);
+            Serial.println(); 
           }
 
-        if(Comp("flo")==0) 
+        if(fl==str) 
           {
+//            Serial.println("out_flo");
             int out_flow=get_flow();
-//            Serial.write(out_flow); 
+            Serial.write(out_flow);
+            Serial.println(); 
           }
  
+    str="";
  }
 
 int get_angle()
@@ -79,7 +72,7 @@ int get_angle()
 int get_length()
 {
   int len;
-  len=1;
+  len=1020;
   return len;
 }
 int get_pres()
