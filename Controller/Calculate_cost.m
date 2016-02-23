109d471ed91be86fd03a2e12c28c75461e636de4
amaidzia <amaidzia@hotmail.com>
2016-02-22 20:58:47 -0500

    Implementation of cost function and controller
    Controller and cost function code written

--- Controller/Calculate_cost.m
+++ Controller/Calculate_cost.m
@@ -15,16 +15,20 @@
 
 function Cost_Outputs = Calculate_cost(Desired_trajectory,Sensor_Data)
 
-    ang =       Sensor_Data(1);
-    length =    Sensor_Data(2);
-    pressure =  Sensor_Data(3);
-    flow =      Sensor_Data(4);
+    ang =        Sensor_Data(1);
+    length1 =    Sensor_Data(2);
+    length2 =    Sensor_Data(3);
+    pressure1 =  Sensor_Data(4);
+    pressure2 =  Sensor_Data(5);
+    %flow =      Sensor_Data(4);
 
     
-    des_ang =       Desired_trajectory(1);
-    des_length =    Desired_trajectory(2);
-    des_pressure =  Desired_trajectory(3);
-    des_flow =      Desired_trajectory(4);
+    des_ang =        Desired_trajectory(1);
+    des_length1 =    Desired_trajectory(2);
+    des_length2 =    Desired_trajectory(3);
+    des_pressure1 =  Desired_trajectory(4);
+    des_pressure =   Desired_trajectory(5);
+%    des_flow =      Desired_trajectory(4);
     
     Cost_Outputs = Desired_trajectory - Sensor_Data;
 
