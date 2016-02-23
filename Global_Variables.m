24b7f8508c46058c58a301e71defd011b24155b9
amaidzia <amaidzia@hotmail.com>
2016-02-22 20:54:23 -0500

    Added some more global variables
    Added some more global variables

--- Global_Variables.m
+++ Global_Variables.m
@@ -10,12 +10,19 @@
 
 
 
-s = serial('COM7');
-set(s,'BaudRate',115200);
-fopen(s);
+% s = serial('COM7');
+% set(s,'BaudRate',115200);
+% fopen(s);
 
 
 global Time_period;
 Time_period = 0.05; % Time period of 1 cycle
+
+global Kp;
+Kp=1; % Kp gain for value open
+
+global M1_expansion_length;
+M1_expansion_length = 0.12; % the length we want M1 to expand to when we are increasing angle and shortening M2
+
 
 
