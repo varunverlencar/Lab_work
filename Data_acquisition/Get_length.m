68e5e2de4d0ad064bb2267ca5d6b35ae939d6a4d
amaidzia <amaidzia@hotmail.com>
2016-02-22 20:56:40 -0500

    Added Serial object finding and muscle number as arguments
    Added Serial object finding and muscle number as arguments. This helps
    reusing of existing function and prevents multiple copies of serial
    objects

--- Data_acquisition/Get_length.m
+++ Data_acquisition/Get_length.m
@@ -7,15 +7,22 @@
 % 
 % *************************************************************************
 
-function length = Get_length()
+function length = Get_length(i)
  out = zeros(1,10);
 
+    s = instrfind('Tag','sweet_serial_of_mine');
+    
     if(isvalid(s)==0)
         fprintf('Invalid serial port, called from get length');
     end
     
-    fprintf(s,'len');
+    if (i == 1)
+    fprintf(s,'len1');
+    elseif(i ==2)
+    fprintf(s,'len2');
+    end
     
+
     out = fscanf(s,'%d',10);
     
     length = length_mapping(out);
--- Data_acquisition/Get_pressure.m
+++ Data_acquisition/Get_pressure.m
@@ -7,15 +7,20 @@
 % 
 % *************************************************************************
 
-function pressure = Get_pressure()
+function pressure = Get_pressure(i)
 
     out = 0;
+    s = instrfind('Tag','sweet_serial_of_mine');
 
     if(isvalid(s)==0)
         fprintf('Invalid serial port, called from get pressure');
     end
     
-    fprintf(s,'pre');
+    if(i == 1)
+    fprintf(s,'pre1');
+	elseif(i == 2)
+	fprintf(s, 'pre2');
+	end
     
     out = fscanf(s,'%d');
     
